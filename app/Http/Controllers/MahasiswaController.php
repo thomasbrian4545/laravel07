6330ed6b788855ab5d35d43e82fa57e6bc38a192
thomasbrian4545 <thomasbrian4545@gmail.com>
2023-09-16 23:20:19 +0700

    Method insertSql()

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MahasiswaController extends Controller
 {
     public function index()
     {
         return "Berhasil di proses";
+    }
+
+    public function insertSql()
+    {
+        $result = DB::insert("INSERT INTO mahasiswas (nim,nama,tanggal_lahir,ipk)
+        VALUES ('19003036','Sari Citra Lestari','2001-12-31',3.5)");
+        dump($result);
     }
 }
